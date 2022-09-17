c4e9b8e905589cf36e5dcab08b1defbddc394a45
mimi <kuni2097@gmail.com>
2022-09-17 22:22:46 +0900

    Initial commit

---
+++ app/src/main/java/com/miminerica/myfirstandroidapp_kuni/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miminerica.myfirstandroidapp_kuni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
